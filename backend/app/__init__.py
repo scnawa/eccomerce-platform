19ffacae5cef2033547fecb6449fbb37dad94edf
Sinha Nawa <scnawa@hotmail.com>
2026-03-30 18:00:24 +1100

    Fixed requirements file, changed init file to check DB funcitonality

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from .config import Config
 from .extensions import db, migrate, jwt
 from app.models import User
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    print("DB URL:", app.config.get("SQLALCHEMY_DATABASE_URI"))
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
